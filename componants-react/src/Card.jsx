877187f644887dff2910b96113ce7d20a2438ccf
shre4ash <shreyashkumbhar9@outlook.com>
2025-06-23 22:16:22 +0530

    acrd componats

---
+++ componants-react/src/Card.jsx
@@ -0,0 +1,16 @@
+function Card(){
+
+    return(
+        <div className="card">
+           <img src=""  alt="Profile picture"></img>
+           <h2>
+            Bro code 
+           </h2>
+           <p>I make websites and play games</p>
+        </div>
+    );
+}
+
+
+
+export default Card
